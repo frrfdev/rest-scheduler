6e71b4d92fbc8444d4f73bb43480e8ff56118a62
Felipe <feliperenanrf23@gmail.com>
2025-01-23 20:02:42 -0300

    add search filter

--- src/features/employees/components/employees.list.tsx
+++ src/features/employees/components/employees.list.tsx
@@ -1,32 +1,41 @@
 import { Loader } from '@/components/ui/loader';
-import { buttonVariants } from '../../../components/ui/button';
-import { cn } from '../../../lib/utils';
+import { Button } from '../../../components/ui/button';
 import { useGetEmployeesQuery } from '../api/query/use-get-employees.query';
 import { EmployeeCard } from './employee.card';
 import { EmployeesForm } from './employees.form';
+import { Input } from '@/components/ui/input';
+import { useState } from 'react';
 
 export const EmployeesList = () => {
   const getEmployeesQuery = useGetEmployeesQuery();
 
+  const [search, setSearch] = useState('');
+
   const employees = getEmployeesQuery.data;
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
 
   return (
     <Loader isVisible={getEmployeesQuery.isLoading}>
       <div className="min-w-[300px]">
         <EmployeesForm>
-          <div
-            className={cn(buttonVariants({ variant: 'success' }), 'w-full')}
-            tabIndex={0}
-          >
+          <Button variant={'success'} className="w-full">
             Create Employee
-          </div>
+          </Button>
         </EmployeesForm>
-        <ul className="flex flex-col gap-2 overflow-y-auto overflow-x-hidden h-full w-full p-4">
-          {employees?.map((employee) => (
-            <li key={employee.id} className="text-white">
-              <EmployeeCard employee={employee} />
-            </li>
-          ))}
+        <Input placeholder="Search" onChange={handleSearch} className="mt-4" />
+        <ul className="flex flex-col gap-2 overflow-y-auto overflow-x-hidden h-full w-full py-4">
+          {employees
+            ?.filter((employee) =>
+              employee.name.toLowerCase().includes(search.toLowerCase())
+            )
+            .map((employee) => (
+              <li key={employee.id} className="text-white">
+                <EmployeeCard employee={employee} />
+              </li>
+            ))}
         </ul>
       </div>
     </Loader>
